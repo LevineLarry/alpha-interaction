06fd2016b6a9f9b0312c12755ad635089768ca48
LevineLarry <larrylevine312@gmail.com>
2022-09-15 21:51:33 -0400

    Navbar upgrades

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -1,6 +1,10 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 const Navbar = () => {
+    const router = useRouter()
+    console.log(router.pathname)
+
     return (
         <div className="w-full py-2 px-36 h-16 z-10 bg-[#0f1b32] flex flex-row justify-between shadow-lg">
             <Link href="/">
@@ -9,13 +13,13 @@ const Navbar = () => {
 
             <div className="flex flex-row gap-10">
                 <Link href="/explore">
-                    <p className="text-white font-[Audiowide] my-auto cursor-pointer transition-all hover:scale-110">Explore</p>
+                    <p className={`${router.pathname == "/explore" ? "text-[#96C6FF]" : "text-white"} font-[Audiowide] my-auto cursor-pointer transition-all hover:scale-110`}>Explore</p>
                 </Link>
                 <Link href="/cloud">
-                    <p className="text-white font-[Audiowide] my-auto cursor-pointer transition-all hover:scale-110">Cloud</p>
+                    <p className={`${router.pathname.includes("/cloud") ? "text-[#96C6FF]" : "text-white"} font-[Audiowide] my-auto cursor-pointer transition-all hover:scale-110`}>Cloud</p>
                 </Link>
                 <Link href="https://proxima-aerospace.square.site">
-                    <p className="text-white font-[Audiowide] my-auto cursor-pointer transition-all hover:scale-110">Store</p>
+                    <p className={`text-white font-[Audiowide] my-auto cursor-pointer transition-all hover:scale-110`}>Store</p>
                 </Link>
             </div>
         </div>
